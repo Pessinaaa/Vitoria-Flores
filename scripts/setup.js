c95246ebf4f7b500320f5424174840a488779c45
Pessinaaa <pedro.pessina99@gmail.com>
2022-06-23 12:32:05 -0300

    :hammer: carrossel de imagens

---
+++ scripts/setup.js
@@ -0,0 +1,16 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    center:true,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
