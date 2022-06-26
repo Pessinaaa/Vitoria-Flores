38fc1368e057a7cc3743a853a6271c1094585590
Pessinaaa <pedro.pessina99@gmail.com>
2022-06-26 17:33:31 -0300

    :hammer: carrossel responsivo

--- scripts/setup.js
+++ scripts/setup.js
@@ -7,6 +7,17 @@ owl.owlCarousel({
     autoplay:true,
     autoplayTimeout:4000,
     autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[1000])
