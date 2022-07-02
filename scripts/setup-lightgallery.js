deb772447932ab84fb1745de4735e3bf87f23781
Pessinaaa <pedro.pessina99@gmail.com>
2022-07-02 16:12:19 -0300

    :wrench: Galerias separadas

--- scripts/setup-lightgallery.js
+++ scripts/setup-lightgallery.js
@@ -1,1 +1,6 @@
-lightGallery(document.querySelector('.gallery'));
+lightGallery(document.querySelector('#galeria-casamento'));
+lightGallery(document.querySelector('#galeria-buque-noiva'));
+lightGallery(document.querySelector('#galeria-buque'));
+lightGallery(document.querySelector('#galeria-decoracao'));
+lightGallery(document.querySelector('#galeria-arranjo'));
+lightGallery(document.querySelector('#galeria-cesta'));
