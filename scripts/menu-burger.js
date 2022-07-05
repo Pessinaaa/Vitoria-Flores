852008d9828a8e220f763af4580eb197b084dc88
Pessinaaa <pedro.pessina99@gmail.com>
2022-07-05 18:29:53 -0300

    :hammer_and_wrench: Melhorias menu mobile

--- scripts/menu-burger.js
+++ scripts/menu-burger.js
@@ -1,7 +1,18 @@
 const iconBurger = document.getElementById('burger')
-const menuList = document.getElementById('menu-list')
 
-iconBurger.addEventListener('click', () => {
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault();
     iconBurger.classList.toggle('fechar')
+    const menuList = document.getElementById('menu-list')
     menuList.classList.toggle('mostrar-menu')
-})
+    const active = menuList.classList.contains('mostrar-menu')
+    event.currentTarget.setAttribute('aria-expanded', active)
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir menu')
+    }
+}
+
+iconBurger.addEventListener('click', toggleMenu);
+iconBurger.addEventListener('touchstart', toggleMenu);
