ecb958c4e7f32cb86271e9786422927408c955fd
Pessinaaa <pedro.pessina99@gmail.com>
2023-06-23 12:05:26 -0300

    :sparkles: Animações no scroll

---
+++ scripts/scroll-reveal.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal({reset: false})
+
+sr.reveal('.title-reveal', {
+    distance: '20%',
+    origin: 'right',
+    duration: 1000
+})
+
+sr.reveal('.reveal', {
+    distance: '20%',
+    origin: 'right',
+    duration: 1000,
+    delay: 500
+})
